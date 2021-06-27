01109b90b5babc3aee54d8b87516d2458f4abd4e
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-27 03:33:42 +0200

    Staged the migrations for Investment model

--- investment_api/models.py
+++ investment_api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class InvestmentModel(models.Model):
+    user_name = models.CharField(max_length=100)
+    amount = models.IntegerField()
+    investment_returns = models.IntegerField()
+    duration = models.IntegerField()
