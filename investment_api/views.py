636758160dffe2e10ae2ee3f94194faa0976ac00
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-28 00:48:23 +0200

    Changed List endpoint to handle multiple http verbs.

--- investment_api/views.py
+++ investment_api/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView,CreateAPIView
+from rest_framework.generics import ListCreateAPIView
 # Create your views here.
 from investment_api.serializers import InvestmentSerializer
 from.models import Investment
 
-class InvestmentsView(ListAPIView):
+class InvestmentsView(ListCreateAPIView):
     serializer_class = InvestmentSerializer
     queryset = Investment.objects.all()
 
