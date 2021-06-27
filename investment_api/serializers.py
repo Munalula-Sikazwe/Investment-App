a227df16f6a06d8b1cebfc6beaacdd9a586bdbe9
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-27 04:00:06 +0200

    Added the class meta to the model serializer.

--- investment_api/serializers.py
+++ investment_api/serializers.py
@@ -2,6 +2,6 @@ from rest_framework.serializers import ModelSerializer
 from .models import Investment as InvestmentModel
 
 class InvestmentSerializer(ModelSerializer):
-    model = InvestmentModel
-    fields = ['id','user_name','amount','duration','investment_returns']
-    
+    class Meta:
+        model = InvestmentModel
+        fields = ['id','user_name','amount','duration','investment_returns']
