2cffb4f6262e0bf506ec46fe111bcee102ceda21
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-27 15:26:55 +0200

    created the Investments View.

--- investment_api/views.py
+++ investment_api/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView
 # Create your views here.
+from investment_api.serializers import InvestmentSerializer
 
+
+class InvestmentsView(ListCreateAPIView):
+    serializer_class = InvestmentSerializer
 
