8ec34cdeffdc68d1659a836be9ced89bc784a95c
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-29 01:49:25 +0200

    Optimized imports from the serializer file.

--- investment_api/serializers.py
+++ investment_api/serializers.py
@@ -1,10 +1,11 @@
-from rest_framework.serializers import ModelSerializer
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, ReadOnlyField
+
 from .models import Investment as InvestmentModel
 
+
 class InvestmentSerializer(ModelSerializer):
-    investmentId = serializers.ReadOnlyField(source='id')
+    investmentId = ReadOnlyField(source='id')
+
     class Meta:
         model = InvestmentModel
-        fields = ['investmentId','username','amount','duration','investmentReturns']
-
+        fields = ['investmentId', 'username', 'amount', 'duration', 'investmentReturns']
