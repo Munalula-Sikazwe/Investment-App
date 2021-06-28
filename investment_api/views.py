1335c9f253133f65c4edced8dc9bd232428027f0
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-29 00:38:17 +0200

    Added feature that calculates investments of users.

--- investment_api/views.py
+++ investment_api/views.py
@@ -1,14 +1,18 @@
-from rest_framework.generics import ListCreateAPIView,DestroyAPIView
-# from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.generics import ListCreateAPIView, DestroyAPIView
 
 # Create your views here.
 from investment_api.serializers import InvestmentSerializer
 from .models import Investment
 
 
+# from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
 class InvestmentsView(ListCreateAPIView):
     serializer_class = InvestmentSerializer
     queryset = Investment.objects.all()
     # permission_classes = [IsAuthenticatedOrReadOnly]
+
+
 class DeleteInvestmentView(DestroyAPIView):
     queryset = Investment.objects.all()
