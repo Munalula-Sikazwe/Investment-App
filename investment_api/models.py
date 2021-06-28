2d22065f2a26aa36465c6b6c4b3a7c830c72e7b6
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-28 23:58:11 +0200

    Added feature that calculates investments of users.

--- investment_api/models.py
+++ investment_api/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
 class Investment(models.Model):
     username = models.CharField(max_length=100)
     amount = models.IntegerField()
-    investmentReturns = models.IntegerField(blank=True,null=True,default=0)
+    investmentReturns = models.IntegerField(blank=True, null=True,editable=False, default=0)
     duration = models.IntegerField()
+
     def __str__(self):
         return self.username
-   
+
+    def calc_returns(self):
+        return self.amount * self.duration * .3
+
+    def save(self, *args, **kwargs):
+        self.investmentReturns = self.calc_returns()
+        super(Investment, self).save(*args, **kwargs)
