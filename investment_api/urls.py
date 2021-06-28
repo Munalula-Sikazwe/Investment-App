b024be74794117f69ac46a0cc32b08a797a4d13e
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-29 00:49:28 +0200

    Strongly typed url kwargs parameter.

--- investment_api/urls.py
+++ investment_api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import InvestmentsView,DeleteInvestmentView
+
+from .views import InvestmentsView, DeleteInvestmentView
+
 urlpatterns = [
-    path('Investments',InvestmentsView.as_view()),
-    path('Investments/Delete/<pk>',DeleteInvestmentView.as_view()),
+    path('Investments', InvestmentsView.as_view()),
+    path('Investments/Delete/<int:pk>', DeleteInvestmentView.as_view()),
 
 ]
