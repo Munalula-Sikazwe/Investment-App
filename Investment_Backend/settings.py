ef450b1ac976d4968748d159dfe40247a77cf375
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-30 23:38:26 +0200

    Changed the allowed host the heroku site

--- Investment_Backend/settings.py
+++ Investment_Backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-cmf6xgn=los57ka))-ml2er)f9=zk5a=dut(f)f^b$8=f4&r6l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://investmentapp-backend.herokuapp.com/","127.0.0.1","localhost"]
+ALLOWED_HOSTS = ["investmentapp-backend.herokuapp.com/","127.0.0.1","localhost"]
 
 
 # Application definition
